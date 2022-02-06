ae7b6d56a8fef9772df3a95c537b913ecb032418
Mika Berglund <mika.berglund@outlook.com>
2022-02-06 22:39:46 +0200

    Added all required Pulumi stuff.

---
+++ PulumiWithAzure/SampleStack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Pulumi;
+using storage = global::Pulumi.AzureNative.Storage;
+
+namespace PulumiWithAzure
+{
+    public class SampleStack : Stack
+    {
+        public SampleStack()
+        {
+            var config = new Config();
+            var rg = config.Require("rg");
+            var prefix = config.Require("prefix");
+
+            var sampleStorage = new storage.StorageAccount($"{prefix}mystorage", new storage.StorageAccountArgs
+            {
+                ResourceGroupName = rg,
+                AccountName = $"{prefix}mystorage",
+                Kind = storage.Kind.StorageV2,
+                Sku = new storage.Inputs.SkuArgs
+                {
+                    Name = storage.SkuName.Standard_GRS
+                }
+            });
+        }
+
+    }
+}
